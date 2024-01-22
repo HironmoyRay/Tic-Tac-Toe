5d75692eeb5e222b207b2a8f7b9c6304c35f752e
hironmoy-ray <hpray35@gmail.com>
2024-01-23 00:06:03 +0600

    Creating Square Component & Use onClick function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-export default function Board(){
-  return(
+function Square({ value }) {
+  function handleClick(){
+    console.log("Clicked from the Square");
+  }
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
     <>
-    <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-    </div>
-    <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-    </div>
-    <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">X</button>
-    </div>
-
-  
+      <div>
+        <Square value="1"></Square>
+        <Square value="2"></Square>
+        <Square value="3"></Square>
+      </div>
+      <div>
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
+      </div>
+      <div>
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
+      </div>
     </>
-
-    )
+  );
 }
