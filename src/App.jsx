47d182c545d5cc6c83b7064faf5e1ccdf2d9a455
hironmoy-ray <hpray35@gmail.com>
2024-01-23 11:44:37 +0600

    Lifting the State Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
-function Square() {
-
-  const [value,setValue] =useState(null);
-  
-  function handleClick() {
-    // console.log("Clicked from the Square");
-    setValue("X");
-  }
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -19,22 +13,56 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // console.log(squares);
+  function handleClick(i) {
+    const newSquares = squares.slice();
+    newSquares[i] = "X";
+    setSquares(newSquares);
+  }
   return (
     <>
       <div className="flex">
-        <Square value="1"></Square>
-        <Square value="2"></Square>
-        <Square value="3"></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square value="4"></Square>
-        <Square value="5"></Square>
-        <Square value="6"></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square value="7"></Square>
-        <Square value="8"></Square>
-        <Square></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
       </div>
     </>
   );
