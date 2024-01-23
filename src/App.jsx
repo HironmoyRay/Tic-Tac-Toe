2eba073700bf57cd8078293ea8cdcaa452b99b0c
hironmoy-ray <hpray35@gmail.com>
2024-01-23 09:27:58 +0600

    Using useState on Square Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function Square({ value }) {
-  function handleClick(){
-    console.log("Clicked from the Square");
+import { useState } from "react";
+
+function Square() {
+
+  const [value,setValue] =useState(null);
+  
+  function handleClick() {
+    // console.log("Clicked from the Square");
+    setValue("X");
   }
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg" onClick={handleClick}>
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
       {value}
     </button>
   );
@@ -12,20 +21,20 @@ function Square({ value }) {
 export default function Board() {
   return (
     <>
-      <div>
+      <div className="flex">
         <Square value="1"></Square>
         <Square value="2"></Square>
         <Square value="3"></Square>
       </div>
-      <div>
+      <div className="flex">
         <Square value="4"></Square>
         <Square value="5"></Square>
         <Square value="6"></Square>
       </div>
-      <div>
+      <div className="flex">
         <Square value="7"></Square>
         <Square value="8"></Square>
-        <Square value="9"></Square>
+        <Square></Square>
       </div>
     </>
   );
